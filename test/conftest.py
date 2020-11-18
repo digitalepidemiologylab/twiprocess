fb310c0bbe3e2c0684794bdb3ffc47d7dc5ac9ac
utanashati <utanashati@mail.ru>
2020-11-18 18:01:17 +0100

    Test fix 3

--- test/conftest.py
+++ test/conftest.py
@@ -6,6 +6,6 @@ from twiprocess.processtweet import ProcessTweet
 
 @pytest.fixture(scope='function')
 def process_tweet():
-    with open('tweet.json', 'r') as f:
+    with open('test/tweet.json', 'r') as f:
         status = json.load(f)
     return ProcessTweet(status)
